d0eb4ff6a4e0033917f2014907cfab030193dc5d
Blaine Howerton <bhowerton@yghdtv.com>
2016-02-16 21:51:02 -0700

    database implementation

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 0) do
-=======
 ActiveRecord::Schema.define(version: 20160215215659) do
 
   create_table "causes", force: :cascade do |t|
