8a5b2d4acc118d970bc7a614e59b81afb7b6a87e
Blaine Howerton <bhowerton@yghdtv.com>
2016-02-19 13:57:54 -0700

    heroku funcitionality

--- Gemfile
+++ Gemfile
@@ -1,11 +1,13 @@
 source 'https://rubygems.org'
 ruby '2.2.4'
 
-
+gem 'rails_12factor'
+gem 'pg'
+gem 'puma'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use mysql2 as the database for Active Record
-gem 'mysql2'
+# gem 'mysql2'
 # Use rails admin for database administration
 gem 'rails_admin'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -13,59 +13,62 @@
 
 ActiveRecord::Schema.define(version: 20160218172939) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "causes", force: :cascade do |t|
-    t.string   "name",          limit: 255
-    t.datetime "created_at",                                                       null: false
-    t.datetime "updated_at",                                                       null: false
-    t.string   "paypalID",      limit: 255
-    t.string   "contact_email", limit: 255
-    t.string   "address",       limit: 255
-    t.string   "city",          limit: 255
+    t.string   "name"
+    t.datetime "created_at",                                                      null: false
+    t.datetime "updated_at",                                                      null: false
+    t.string   "paypalID"
+    t.string   "contact_email"
+    t.string   "address"
+    t.string   "city"
     t.string   "state",         limit: 2
     t.string   "zip",           limit: 5
     t.string   "phone",         limit: 10
-    t.decimal  "split",                     precision: 5, scale: 2, default: 0.03, null: false
-    t.decimal  "balance",                   precision: 5, scale: 2, default: 0.0,  null: false
+    t.decimal  "split",                    precision: 5, scale: 2, default: 0.03, null: false
+    t.decimal  "balance",                  precision: 5, scale: 2, default: 0.0,  null: false
   end
 
   create_table "retailers", force: :cascade do |t|
-    t.string   "name",          limit: 255
-    t.datetime "created_at",                                                       null: false
-    t.datetime "updated_at",                                                       null: false
-    t.string   "paypalID",      limit: 255
-    t.string   "address",       limit: 255
-    t.string   "city",          limit: 255
+    t.string   "name"
+    t.datetime "created_at",                                                      null: false
+    t.datetime "updated_at",                                                      null: false
+    t.string   "paypalID"
+    t.string   "address"
+    t.string   "city"
     t.string   "state",         limit: 2
     t.string   "zip",           limit: 5
     t.string   "phone",         limit: 10
-    t.string   "contact_email", limit: 255
-    t.decimal  "split",                     precision: 5, scale: 2, default: 0.03, null: false
-    t.decimal  "balance",                   precision: 5, scale: 2, default: 0.0,  null: false
+    t.string   "contact_email"
+    t.decimal  "split",                    precision: 5, scale: 2, default: 0.03, null: false
+    t.decimal  "balance",                  precision: 5, scale: 2, default: 0.0,  null: false
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.decimal  "amount",                     precision: 10, scale: 2
-    t.integer  "user_id",          limit: 4
-    t.integer  "retailer_id",      limit: 4
-    t.datetime "created_at",                                          null: false
-    t.datetime "updated_at",                                          null: false
+    t.decimal  "amount",           precision: 10, scale: 2
+    t.integer  "user_id"
+    t.integer  "retailer_id"
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.boolean  "approved"
     t.date     "transaction_date"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",      limit: 255
-    t.integer  "cause_id",      limit: 4
-    t.datetime "created_at",                                                      null: false
-    t.datetime "updated_at",                                                      null: false
-    t.string   "paypalID",      limit: 255
-    t.string   "address",       limit: 255
-    t.string   "city",          limit: 255
+    t.string   "username"
+    t.integer  "cause_id"
+    t.datetime "created_at",                                                     null: false
+    t.datetime "updated_at",                                                     null: false
+    t.string   "paypalID"
+    t.string   "address"
+    t.string   "city"
     t.string   "state",         limit: 2
     t.string   "zip",           limit: 5
     t.string   "phone",         limit: 10
-    t.string   "contact_email", limit: 255
-    t.decimal  "balance",                   precision: 5, scale: 2, default: 0.0, null: false
+    t.string   "contact_email"
+    t.decimal  "balance",                  precision: 5, scale: 2, default: 0.0, null: false
   end
 
 end
