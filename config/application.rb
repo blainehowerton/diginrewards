192eb2e48686a40f1cb031a7849de45cdd33b94a
Blaine Howerton <bhowerton@yghdtv.com>
2016-02-19 15:19:45 -0700

    revised gemfile

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# Prevent initializing the application before assets are precompiled (required for heroku) 
-config.assets.initialize_on_precompile = false 
-
-# Add Rails Admin assets (required)
- config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
-
 
 module Dig
   class Application < Rails::Application
