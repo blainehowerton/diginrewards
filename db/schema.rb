60c741c009e99e26ec3bbe66b7cdf20c13c7b6cd
Blaine Howerton <bhowerton@yghdtv.com>
2016-03-16 22:59:03 -0600

    db:reload

--- db/schema.rb
+++ db/schema.rb
@@ -73,16 +73,6 @@ ActiveRecord::Schema.define(version: 20160315032204) do
     t.string   "state",                  limit: 2
     t.string   "zip",                    limit: 5
     t.string   "phone",                  limit: 10
-    t.string   "email",                             default: "", null: false
-    t.string   "encrypted_password",                default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                     default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
   end
 
 end
