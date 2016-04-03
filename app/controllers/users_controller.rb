c63825ba6fbdcdeeb069a9b07feafba7f0de971e
Blaine Howerton <bhowerton@yghdtv.com>
2016-04-03 10:09:23 -0600

    created new user view in onboarding.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  def index
+	def index
   	@users = User.all
-  	end
+	end
 
 	def new
     @user = User.new
@@ -13,6 +13,21 @@ class UsersController < ApplicationController
 	end
 
 	def create
+	@user = User.new(user_params)
+
+		respond_to do |format|
+	      if @user.save
+	        format.html { redirect_to '/onboarding', notice: 'User was successfully created.' }
+	        format.json { render :show, status: :created, location: @user }
+	      else
+	        format.html { render :new }
+	        format.json { render json: @user.errors, status: :unprocessable_entity }
+	      end
+	    end
 	end
+
+	def retailer_params
+      params.require(:user).permit(:username, :cause_id, :paypalID, :street_address, :city, :state, :zip, :phone, :email, :encrypted_password)
+    end
 
 end
