7e201b7b0d8225587ab0da17443504fb44864f53
Blaine Howerton <bhowerton@yghdtv.com>
2016-04-03 00:01:38 -0600

    added validation for onboarding views

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,3 +1,8 @@
 class Cause < ActiveRecord::Base
   has_many :users
+
+  validates_presence_of :name, :message => "Cause name must be entered."
+  validates_presence_of :paypalID, :message => "Pay pal ID must be entered."
+  validates_presence_of :zip, :message => "Zip code must be entered."
+
 end
