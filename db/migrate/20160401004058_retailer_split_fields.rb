fda6ca7de64dddd30db50a2154e3ba130d2129c3
Blaine Howerton <bhowerton@yghdtv.com>
2016-04-15 15:02:46 -0600

    collapsed migrations and created a starting structure

--- db/migrate/20160401004058_retailer_split_fields.rb
+++
@@ -1,7 +0,0 @@
-class RetailerSplitFields < ActiveRecord::Migration
-  def change
-  	rename_column("retailers", "split", "fee_split")
-  	add_column :retailers, :cause_split, :decimal, precision: 5, scale: 2
-  	add_column :retailers, :user_split, :decimal, precision: 5, scale: 2
-  end
-end
