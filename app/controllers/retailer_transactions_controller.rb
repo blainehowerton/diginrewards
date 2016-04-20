5cb1c979828b27afd662de3e836fb1e5d73e3346
Blaine Howerton <bhowerton@yghdtv.com>
2016-04-19 20:59:49 -0600

    added balance calculation field to the retailer index view

--- app/controllers/retailer_transactions_controller.rb
+++ app/controllers/retailer_transactions_controller.rb
@@ -4,7 +4,7 @@ class RetailerTransactionsController < ApplicationController
 def index
     # @retailer_debits = RetailerTransaction.group('retailer_id').sum(:debit_amount).sum(:credit_amount)
     @retailer_balances = RetailerTransaction.group(:retailer_id).select("retailer_id, SUM(retailer_transactions.debit_amount) AS total_debits, SUM(retailer_transactions.credit_amount) AS total_credits")
-    @retailers = Retailer.all
+    @retailer = Retailer.new
 end
 
 def new
