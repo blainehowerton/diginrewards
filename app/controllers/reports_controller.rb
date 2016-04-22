8b2e6fc3142439bfb37d670b8630ad7f99e07e75
Ninjef <jeff.arnold@welltok.com>
2016-04-21 20:19:56 -0600

    update retailer report query

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,7 +4,6 @@ class ReportsController < ApplicationController
   end
 
   def retailer_balances
-    # @retailer_debits = RetailerTransaction.group('retailer_id').sum(:debit_amount).sum(:credit_amount)
-    @retailer_balances = RetailerTransaction.group(:retailer_id).select("retailer_id, SUM(retailer_transactions.debit_amount) AS total_debits, SUM(retailer_transactions.credit_amount) AS total_credits")
+    @retailer_balances = RetailerTransaction.joins(:retailer).group(:retailer_id).select("retailer_id, SUM(retailer_transactions.debit_amount) AS total_debits, SUM(retailer_transactions.credit_amount) AS total_credits")
   end
 end
