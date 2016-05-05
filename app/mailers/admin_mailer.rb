a0b00f066015ef5438fbe0bd14834cc1d0ec6b1a
Blaine Howerton <bhowerton@yghdtv.com>
2016-05-04 22:24:37 -0600

    set up admin email delivery:

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,7 +2,6 @@ class AdminMailer < ApplicationMailer
 
 def admin_email(user)
 	@user = user
-	@url  = 'http://example.com/login'
     mail(to: "bhowerton@yghdtv.com", subject: 'Welcome to My Awesome Site')
   end
 end
