921fb8e46992dbdf2e8f7eae6b60964743d1dcb5
Blaine Howerton <bhowerton@yghdtv.com>
2016-05-04 23:27:57 -0600

    revised admin email view to indicate sign up fields

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,6 @@ class AdminMailer < ApplicationMailer
 
 def admin_email(user)
 	@user = user
-    mail(to: "bhowerton@yghdtv.com", subject: 'Welcome to My Awesome Site')
+    mail(to: "bhowerton@yghdtv.com, diginco@gmail.com", subject: 'Dig In Rewards Notification')
   end
 end
