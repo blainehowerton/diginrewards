885fc4116b2b9d0aad51456b2e07fe27b2af7b21
Blaine Howerton <bhowerton@yghdtv.com>
2016-05-06 00:22:24 -0600

    revised controller database query and view.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,9 @@
 class ReportsController < ApplicationController
+  
   def index
-
   end
 
   def retailer_balances
-    @retailer_balances = RetailerTransaction.joins(:retailer).group(:retailer_id).select("retailer_id, SUM(retailer_transactions.debit_amount) AS total_debits, SUM(retailer_transactions.credit_amount) AS total_credits")
+    @retailer_balances = RetailerTransaction.joins(:retailer).group(:retailer_id).select("retailer_id, SUM(retailer_transactions.debit_amount) AS total_debits, SUM(retailer_transactions.credit_amount) AS total_credits, SUM(retailer_transactions.credit_amount) - SUM(retailer_transactions.debit_amount) AS balance")
   end
 end
